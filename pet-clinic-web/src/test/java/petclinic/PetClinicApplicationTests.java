3103f6618dcdbc99f77bad86bdee000d818524bf
Anton Yekarashev <cherrydonut777@gmail.com>
2023-10-02 16:23:52 +0300

    Changes to test class.

--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -1,9 +1,12 @@
 package petclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PetClinicApplicationTests.class)
 class PetClinicApplicationTests {
 
 	@Test
